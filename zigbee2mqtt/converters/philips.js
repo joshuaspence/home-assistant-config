a10cd8ac80704b84e405bc5fe16067ebba0942fe
Joshua Spence <josh@spence.com.au>
2022-03-03 13:56:15 +1100

    Use `eslint-config-airbnb-base` instead of `eslint-config-google`

--- zigbee2mqtt/converters/philips.js
+++ zigbee2mqtt/converters/philips.js
@@ -1,12 +1,12 @@
-const {exposes} = require('zigbee-herdsman-converters');
-const {extendDevice} = require('./utils');
+const { exposes } = require('zigbee-herdsman-converters');
+const { extendDevice } = require('./utils');
 
 const e = {
   hue_power_on_behavior: () => exposes.enum('power_on_behavior', exposes.access.ALL, ['off', 'recover', 'on'])
-      .withProperty('hue_power_on_behavior')
-      .withDescription('Controls the behavior when the device is powered on'),
+    .withProperty('hue_power_on_behavior')
+    .withDescription('Controls the behavior when the device is powered on'),
 };
 
 module.exports = [
-  extendDevice('LOM005', {exposes: [e.hue_power_on_behavior()]}),
+  extendDevice('LOM005', { exposes: [e.hue_power_on_behavior()] }),
 ];
