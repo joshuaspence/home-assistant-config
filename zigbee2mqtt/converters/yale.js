44454274420db8682bdbf8eea4d3d71fcdcb4bb8
Joshua Spence <josh@spence.com.au>
2023-01-30 15:31:10 +1100

    Add `battery_low` to Yale Assure locks

--- zigbee2mqtt/converters/yale.js
+++ zigbee2mqtt/converters/yale.js
@@ -4,6 +4,6 @@ const { e, tz } = require('./zhc');
 module.exports = [
   extendDevice('YRD226 TSDB', {
     toZigbee: [tz.lock_auto_relock_time, tz.lock_sound_volume],
-    exposes: [e.auto_relock_time(), e.sound_volume()],
+    exposes: [e.auto_relock_time(), e.sound_volume(), e.battery_low()],
   }),
 ];
