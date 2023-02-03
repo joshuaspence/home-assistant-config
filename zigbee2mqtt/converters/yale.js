2725aac0fd0a60c17ca9e4a5102560b7c08a827d
Joshua Spence <josh@spence.com.au>
2023-02-04 09:47:51 +1100

    Add `actuatorEnabled` to Yale Assure locks

--- zigbee2mqtt/converters/yale.js
+++ zigbee2mqtt/converters/yale.js
@@ -1,7 +1,25 @@
 const { extendDevice } = require('./utils');
-const { ea, exposes, utils } = require('./zhc');
+const {
+  constants,
+  ea,
+  exposes,
+  reporting,
+  utils,
+} = require('./zhc');
 
 const fzLocal = {
+  actuator_enabled: {
+    cluster: 'closuresDoorLock',
+    type: ['attributeReport', 'readResponse'],
+    convert: (model, msg, publish, options, meta) => {
+      if (msg.data.hasOwnProperty('actuatorEnabled')) {
+        return { actuator_enabled: !!msg.data.actuatorEnabled };
+      }
+
+      return {};
+    },
+  },
+
   inside_status_led: {
     cluster: 'closuresDoorLock',
     type: ['attributeReport', 'readResponse'],
@@ -64,6 +82,13 @@ const fzLocal = {
 };
 
 const tzLocal = {
+  actuator_enabled: {
+    key: ['actuator_enabled'],
+    convertGet: async (entity, key, meta) => {
+      await entity.read('closuresDoorLock', ['actuatorEnabled']);
+    },
+  },
+
   inside_status_led: {
     key: ['inside_status_led'],
     convertSet: async (entity, key, value, meta) => {
@@ -122,16 +147,23 @@ const tzLocal = {
 };
 
 module.exports = [
-  // TODO: Add `actuatorEnabled` and `operatingMode`.
+  // TODO: Add `operatingMode`.
   extendDevice('YRD226 TSDB', {
     fromZigbee: Object.values(fzLocal),
     toZigbee: Object.values(tzLocal),
     exposes: [
+      exposes.binary('actuator_enabled', ea.STATE_GET, true, false).withDescription('Whether the actuator is enabled'),
       exposes.binary('inside_status_led', ea.ALL, true, false).withDescription('Enable/disable inside status LED'),
       exposes.enum('language', ea.ALL, ['en', 'es', 'fr']).withDescription('Device language'),
       exposes.binary('one_touch_locking', ea.ALL, true, false).withDescription('Enable/disable one-touch locking'),
       exposes.binary('privacy_mode_button', ea.ALL, true, false).withDescription('Enable/disable privacy mode button'),
       exposes.numeric('wrong_code_entry_limit', ea.ALL).withValueMin(0).withValueMax(255).withDescription('Wrong code entry limit'),
     ],
+    configure: async (device, coordinatorEndpoint, logger) => {
+      const endpoint = device.getEndpoint(1);
+
+      const payload = reporting.payload('actuatorEnabled', constants.repInterval.HOUR, constants.repInterval.MAX, 0);
+      await endpoint.configureReporting('closuresDoorLock', payload);
+    },
   }),
 ];
--- zigbee2mqtt/converters/zhc.js
+++ zigbee2mqtt/converters/zhc.js
@@ -1,8 +1,10 @@
 const zhc = require('zigbee-herdsman-converters');
+const constants = require('zigbee-herdsman-converters/lib/constants');
 const reporting = require('zigbee-herdsman-converters/lib/reporting');
 const utils = require('zigbee-herdsman-converters/lib/utils');
 
 module.exports = {
+  constants,
   e: zhc.exposes.presets,
   ea: zhc.exposes.access,
   exposes: zhc.exposes,
