4cc1855ff14b9326776870e8c0bf374b6db94368
Joshua Spence <josh@spence.com.au>
2023-02-03 13:40:08 +1100

    Add `language` to Yale Assure Locks

--- zigbee2mqtt/converters/yale.js
+++ zigbee2mqtt/converters/yale.js
@@ -14,6 +14,18 @@ const fzLocal = {
     },
   },
 
+  language: {
+    cluster: 'closuresDoorLock',
+    type: ['attributeReport', 'readResponse'],
+    convert: (model, msg, publish, options, meta) => {
+      if (msg.data.hasOwnProperty('language')) {
+        return { language: msg.data.language };
+      }
+
+      return {};
+    },
+  },
+
   one_touch_locking: {
     cluster: 'closuresDoorLock',
     type: ['attributeReport', 'readResponse'],
@@ -52,6 +64,18 @@ const tzLocal = {
     },
   },
 
+  language: {
+    key: ['language'],
+    convertSet: async (entity, key, value, meta) => {
+      utils.validateValue(value, ['en', 'es', 'fr']);
+      await entity.write('closuresDoorLock', { language: value }, utils.getOptions(meta.mapped, entity));
+      return { state: { [key]: value } };
+    },
+    convertGet: async (entity, key, meta) => {
+      await entity.read('closuresDoorLock', ['language']);
+    },
+  },
+
   one_touch_locking: {
     key: ['one_touch_locking'],
     convertSet: async (entity, key, value, meta) => {
@@ -77,10 +101,11 @@ const tzLocal = {
 
 module.exports = [
   extendDevice('YRD226 TSDB', {
-    fromZigbee: [fzLocal.inside_status_led],
-    toZigbee: [tzLocal.inside_status_led],
+    fromZigbee: Object.values(fzLocal),
+    toZigbee: Object.values(tzLocal),
     exposes: [
       exposes.binary('inside_status_led', ea.ALL, true, false).withDescription('Enable/disable inside status LED'),
+      exposes.enum('language', ea.ALL, ['en', 'es', 'fr']).withDescription('Device language'),
       exposes.binary('one_touch_locking', ea.ALL, true, false).withDescription('Enable/disable one-touch locking'),
       exposes.binary('privacy_mode_button', ea.ALL, true, false).withDescription('Enable/disable privacy mode button'),
     ],
